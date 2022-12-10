ce4c8849ac758975fc3cf1be1df895fb8ffcb5d0
Charles Li <43394922+lyc-king@users.noreply.github.com>
2022-12-09 21:36:31 -0800

    Update the IK by algebriac equations

--- robot_IK.py
+++ robot_IK.py
@@ -8,76 +8,55 @@ def ik (targXYZ, Mode):
     x = targXYZ[0]; y = targXYZ[1]; z = targXYZ[2]
 
     if Mode == 1: # Horizontal Case
-        ####### Geometric
-        theta1 = math.atan2(y, x)
-        r1 = math.sqrt(x ** 2 + y ** 2) - L4
-        r2 = math.sqrt(r1 ** 2 + (z - L1) ** 2)
 
-        c3 = (r2 ** 2 - L2 ** 2 - L3 ** 2) / (2 * L2 * L3)
-        s3 = -math.sqrt(1 - c3 ** 2); # Two Solns: s3 = math.sqrt(1 - c3 ** 2)
-
-        theta3 = math.atan2(s3, c3)
-
-        alpha = math.atan2(z - L1, r1)
-        cb = (L2 ** 2 + r2 ** 2 - L3 ** 2) / (2 * L2 * r2)
-        sb = - L3 * s3 / r2
-        beta = math.atan2(sb, cb)
-        theta2 = alpha + beta
-
-        theta4 = - theta2 - theta3
-        
-        ###################### Algebraic
+######################
         theta1 = math.atan2(y,x)
     
-        c3 = (x^2 + y^2 + z^2 + L1^2 + L4^2 - L2^2 - L3^2 - 2 * z * L1 - 2 * L4 * np.sqrt(x^2 + y^2)) / (2 * L2 * L3)
-        s3 = -np.sqrt(1 - c3^2); # Two Solns: s3 = sqrt(1 - c3^2)
+        c3 = (x ** 2 + y ** 2 + z ** 2 + L1 ** 2 + L4 ** 2 - L2 ** 2 - L3 ** 2 - 2 * z * L1 - 2 * L4 * np.sqrt(x ** 2 + y ** 2)) / (2 * L2 * L3)
+        s3 = -np.sqrt(1 - c3 ** 2); # Two Solns: s3 = sqrt(1 - c3 ** 2)
         theta3 = math.atan2(s3,c3)
         
-        c2 = (L2 + L3 * c3) * (np.sqrt(x^2 + y^2) - L4) + L3 * s3 * (z - L1)
-        s2 = (L2 + L3 * c3) * (z - L1) - L3 * s3 * (np.sqrt(x^2 + y^2) - L4)
+        c2 = (L2 + L3 * c3) * (np.sqrt(x ** 2 + y ** 2) - L4) + L3 * s3 * (z - L1)
+        s2 = (L2 + L3 * c3) * (z - L1) - L3 * s3 * (np.sqrt(x ** 2 + y ** 2) - L4)
         theta2 = math.atan2(s2,c2)
         
         theta4 = - theta2 - theta3
 
-        ########################
+########################
 
     elif Mode == 2: # Vertical Case
-        theta1 = math.atan2(y, x)
-        r1 = math.sqrt(x ** 2 + y ** 2)
-        r2 = math.sqrt(r1 ** 2 + (z - L1 + L4) ** 2)
 
-        c3 = (r2 ** 2 - L2 ** 2 - L3 ** 2) / (2 * L2 * L3)
-        s3 = -math.sqrt(1 - c3 ** 2); # Two Solns: s3 = sqrt(1 - c3^2)
+        theta1 = math.atan2(y,x)
+    
+        c3 = (x ** 2 + y ** 2 + z ** 2 - L2 ** 2 - L3 ** 2 + (L1 - L4) ** 2 + 2 * z * (L4 - L1)) / (2 * L2 * L3)
+        s3 = -np.sqrt(1 - c3 ** 2); # Two Solns: s3 = sqrt(1 - c3 ** 2)
+        theta3 = math.atan2(s3,c3)
+        
+        c2 = (L2 + L3 * c3) * np.sqrt(x ** 2 + y ** 2) + L3 * s3 * (z - L1 + L4)
+        s2 = (L2 + L3 * c3) * (z - L1 + L4) - L3 * s3 * np.sqrt(x ** 2 + y ** 2)
+        theta2 = math.atan2(s2,c2)
 
-        theta3 = math.atan2(s3, c3)
-
-        alpha = math.atan2(z - L1 + L4, r1)
-        cb = (L2 ** 2 + r2 ** 2 - L3 ** 2) / (2 * L2 * r2)
-        sb = - L3 * s3 / r2
-        beta = math.atan2(sb, cb)
-        theta2 = alpha + beta
-
-        theta4 = - math.pi / 2 - theta2 - theta3
+        theta4 = - np.pi / 2 - theta2 - theta3
 
     else:
         print('Unauthorized Mode?')
 
+    # Calculate jAng for motors as int
     jAng_temp = np.array([theta1, theta2, theta3, theta4])
-    
-    jAng = np.round(jAng_temp / math.pi * 2048 + 2048)
+    jAng = np.round(jAng_temp / np.pi * 2048 + 2048).astype(int)
+
     # output jAng = [t1, t2, t3, t4],  0 < t1,t2,t3,t4 < 4095
     jAng = jAng.tolist()
-    # Assume jAng is angles within the range of [-pi, pi] <--> [0, 4095] ?    
 
     return jAng 
 
 
 # Test Only
 if __name__ == '__main__':
-    c = [0.085, 0.130, 0.130, 0.004] # Length Parameters of Manipulator (verified already)
-    Mode = 1 # or 2
-    targXYZ = np.array([0.15, 0, 0.08]) # Attention: workspace limit
-    jAng = ik (targXYZ, Mode)
+    c = [0.10, 0.13, 0.13, 0.06] # Length Parameters of Manipulator
+    Mode = 2 # or 2
+    targXYZ = np.array([0.15, 0, 0.08])
+    jAng = ik(targXYZ, Mode)
     print(jAng)
     print(type(jAng))
     
