158a66cea34213eb4b9644156e548ed4e855c504
Bruce <48899125+bruce0902@users.noreply.github.com>
2022-12-09 19:19:50 -0800

    Final Code

--- test.py
+++ test.py
@@ -2,7 +2,26 @@ import os
 from robot_FK import *
 from robot_IK import *
 import time
+import math
+import numpy as py
 
+
+def homegoto(P_start, home, N = 101):
+    Px = P_start[0]; Py = P_start[1]; Pz = P_start[2]
+    Hx = home[0]; Hy = home[1]; Hz = home[2]
+
+    # Goal Trajectory: Cosine Function --- z = Acos(wt)+A
+    T = np.sqrt((Px - Hx) ** 2 + (Py - Hy) ** 2); # T is half period of Cosine Function
+    t = np.linspace(0,T,N)
+    A = (Hz - Pz) / 2
+    w = np.pi / T
+
+    x = np.linspace(Hx, Px, N)
+    y = np.linspace(Hy, Py, N)
+    z = Pz + A * np.cos(w * t) + A
+    traj = np.vstack((x, y, z))
+
+    return traj
 # Dynamixel Initialization
 if os.name == 'nt':
     import msvcrt
@@ -100,40 +119,110 @@ for ID in DXL_ID:
 param_jAng = [[0] * 4] * len(DXL_ID) # 2D array for position val for each joint
 cur_jAng = [90, 90, 90, 90]
 
-a=2048
-flag=1
+# a=2048
+# b=2048
+# c=3072
+# flag=1
+# x=0
+# y=0
+# z=30
 
 ### Traj
 # N=len
+        # Bulkread present position and LED status
+dxl_comm_result = groupBulkRead.txRxPacket()
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+for ID in DXL_ID:  # Check data available
+        dxl_getdata_result = groupBulkRead.isAvailable(ID, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
+        if dxl_getdata_result != True:
+            print("[ID:%03d] groupBulkRead getdata failed" % ID)
+            quit()
+        # Read j angle data
+        cur_jAng[ID-1] = groupBulkRead.getData(ID, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
 
+N = 101
+M = 10
+#home_position=np.array([0.06,0,0.36])
+home_position = fk(cur_jAng)
+print(home_position)
+starting_point=np.array([0.1789,0.0289,0.1089])
+traj=homegoto(starting_point,home_position)
+
+film_point=0.1089
+
+# for i in range(N):
+#     film_point=np.array([0.1789+0.01*i,0.0289,0.1089])
+# trajfilm=
+
+jAng =[0, 0, 0 ,0]
+for i in range(N+M):
+    if i < N:
+        goal = [traj[0][i], traj[1][i],traj[2][i]]
+        jAng = np.vstack((jAng,ik(goal,Mode=1)))
+    else:
+        goal = [0.1789 + 0.008 * (i-N), 0.0289, 0.1089]
+        print(goal)
+        # jAng = np.vstack((jAng,ik(goal,Mode=1)))
+        
+
+
+# jAng_film = [0, 0, 0 ,0]
+#     for i in range(N):
+
+
+print(jAng)
+print(len(jAng))
+# print(traj)
+run=1
 while 1:
 
     # if getch() == chr(0x1b): # ESC to break loop
     #     break
     
-    if(flag==1):
+    # print(flag)
+    # print(a)
+    # if(flag==1):
 
-    #     # goal = [2048, 3072, 2048, 2048] 
-    #     # goal = goal.split(" ")
-    #     # for j in range(4):
-    #     #     goal[j] = int(goal[j])
-    #     # target_jAng = goal
-        target_jAng = [2048, 3072, 2048, 2048]  # Home Position
-        flag = 2
+    # # #     # goal = [2048, 3072, 2048, 2048] 
+    # # #     # goal = goal.split(" ")
+    # # #     # for j in range(4):
+    # # #     #     goal[j] = int(goal[j])
+    # # #     # target_jAng = goal
+    #     target_jAng = [2048, 3072, 2048, 2048]  # Home Position
+    #     flag = 2
+    #     print(flag)
+    #     print(target_jAng)
 
+    # else:
+    # if(a<2600):
+    #     a=a-5
+    #     b=b+5
+    #     c=c-5
+    # else:
+    #     break
+    # # target_jAng= [2048,c,b,a] # IK(x,y,z)
+    # print(target_jAng)
+    target_jAng=[0,0,0,0]
+    
+    target_jAng[0]=int(jAng[run][0])
+    target_jAng[1]=int(jAng[run][1])
+    target_jAng[2]=int(jAng[run][2])
+    target_jAng[3]=int(jAng[run][3])
+    print(type(target_jAng))
+    print(type(target_jAng[0]))
+    if(run == N):
+        break
     else:
-
-        if(a<2600):
-            a=a+10
-        else:
-            break
-        target_jAng= [2048,3072,2048,a] # IK(x,y,z)
+        run=run+1
+    # target_jAng=ik(traj)
 
 
 
     for i in range(len(DXL_ID)):
         # format each joint angle to byte array
         ang = target_jAng[i]
+        # ang = target_jAng[i]
         param_jAng[i] = [DXL_LOBYTE(DXL_LOWORD(ang)), DXL_HIBYTE(DXL_LOWORD(ang)), DXL_LOBYTE(DXL_HIWORD(ang)), DXL_HIBYTE(DXL_HIWORD(ang))]
         # Add Dynamixel#1 goal position value to the Bulkwrite parameter storage
         dxl_addparam_result = groupBulkWrite.addParam(DXL_ID[i], ADDR_GOAL_POSITION, LEN_GOAL_POSITION, param_jAng[i])
@@ -153,36 +242,45 @@ while 1:
     # Clear bulkwrite parameter storage
     groupBulkWrite.clearParam()
 
-    while 1:  # Reading data until moved to position
-        # Bulkread present position and LED status
-        dxl_comm_result = groupBulkRead.txRxPacket()
-        if dxl_comm_result != COMM_SUCCESS:
-            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    # for i in range(100):
 
-        for ID in DXL_ID:  # Check data available
-            dxl_getdata_result = groupBulkRead.isAvailable(ID, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
-            if dxl_getdata_result != True:
-                print("[ID:%03d] groupBulkRead getdata failed" % ID)
-                quit()
-            # Read j angle data
-            cur_jAng[ID-1] = groupBulkRead.getData(ID, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
-            print("[ID:%03d] Present Position : %d \t" % (ID, cur_jAng[ID-1]))
-        state = 0
-        for i in range(len(DXL_ID)):  # check if all joints already moved to position
-            if abs(cur_jAng[i] - target_jAng[i]) < DXL_MOVING_STATUS_THRESHOLD:
-                state = state + 1
-            print(i)
+    time.sleep(0.05)
+
+    if run == N:
+        time.sleep(3)
+    # while 1:  # Reading data until moved to position
+    #     # Bulkread present position and LED status
+    #     dxl_comm_result = groupBulkRead.txRxPacket()
+    #     if dxl_comm_result != COMM_SUCCESS:
+    #         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+
+    #     for ID in DXL_ID:  # Check data available
+    #         dxl_getdata_result = groupBulkRead.isAvailable(ID, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
+    #         if dxl_getdata_result != True:
+    #             print("[ID:%03d] groupBulkRead getdata failed" % ID)
+    #             quit()
+    #         # Read j angle data
+    #         cur_jAng[ID-1] = groupBulkRead.getData(ID, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION)
+    #         print("[ID:%03d] Present Position : %d \t" % (ID, cur_jAng[ID-1]))
+    #     state = 0
+    #     for i in range(len(DXL_ID)):  # check if all joints already moved to position
+    #         if abs(cur_jAng[i] - target_jAng[i]) < DXL_MOVING_STATUS_THRESHOLD:
+    #             state = state + 1
+    #             break
+    #         print(i)
                 
-        if state == len(DXL_ID):
-            print('Reading Done!')
-            break
-    # ball_last_pos = ball_pos
-    # end_t = time.time()
+    #     if state == len(DXL_ID):
+    #         print('Reading Done!')
+    #         break
+    # # ball_last_pos = ball_pos
+    # # end_t = time.time()
     # dt = end_t - start_t
 ########################################################################################################################
 
 # Clear bulkread parameter storage
 groupBulkRead.clearParam()
+
+time.sleep(5)
 
 # Disable Dynamixel#1 Torque
 dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL1_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE)
@@ -216,3 +314,5 @@ elif dxl_error != 0:
 
 # Close port
 portHandler.closePort()  
+
+
